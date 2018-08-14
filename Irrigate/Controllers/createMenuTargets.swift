55d37c96616488d37f1018178d571fe8f08ddf2b
Dom Clemmer <thisisdom4@gmail.com>
2018-08-14 19:03:43 -0400

    add function for adding menu cones

--- Irrigate/Controllers/createMenuTargets.swift
+++ Irrigate/Controllers/createMenuTargets.swift
@@ -6,4 +6,44 @@
 //  Copyright © 2018 IrrigateMsg. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import SceneKit
+import ARKit
+
+func createMenuTargets(scene:ARSCNView) {
+    
+    let greenTarget = makeLargeCone()
+    greenTarget.position = SCNVector3(-1,0,3)
+    greenTarget.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: greenTarget, options: [SCNPhysicsShape.Option.scale: 0.25]))
+    greenTarget.name = "startCone"
+    greenTarget.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+    
+    greenTarget.physicsBody?.categoryBitMask = BitMaskCategory.startConeCategory.rawValue
+    greenTarget.physicsBody?.collisionBitMask = BitMaskCategory.floorCategory.rawValue | BitMaskCategory.ballCategory.rawValue
+    greenTarget.physicsBody?.contactTestBitMask = BitMaskCategory.ballCategory.rawValue
+    
+    let redTarget = makeLargeCone()
+    redTarget.position = SCNVector3(-3,0,3)
+    redTarget.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: redTarget, options: [SCNPhysicsShape.Option.scale: 0.25]))
+    redTarget.name = "quitTarget"
+    redTarget.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+    
+    redTarget.physicsBody?.categoryBitMask = BitMaskCategory.quitCategory.rawValue
+    redTarget.physicsBody?.collisionBitMask = BitMaskCategory.floorCategory.rawValue | BitMaskCategory.ballCategory.rawValue
+    redTarget.physicsBody?.contactTestBitMask = BitMaskCategory.ballCategory.rawValue
+    
+    let purpleTarget = makeLargeCone()
+    purpleTarget.position = SCNVector3(-2,0,3)
+    purpleTarget.physicsBody = SCNPhysicsBody(type: .dynamic, shape: SCNPhysicsShape(node: purpleTarget, options: [SCNPhysicsShape.Option.scale: 0.25]))
+    purpleTarget.name = "trainTarget"
+    purpleTarget.geometry?.firstMaterial?.diffuse.contents = UIColor.purple
+    
+    purpleTarget.physicsBody?.categoryBitMask = BitMaskCategory.startTrainingCategory.rawValue
+    purpleTarget.physicsBody?.collisionBitMask = BitMaskCategory.floorCategory.rawValue | BitMaskCategory.ballCategory.rawValue
+    purpleTarget.physicsBody?.contactTestBitMask = BitMaskCategory.ballCategory.rawValue
+    
+    scene.scene.rootNode.addChildNode(greenTarget)
+    scene.scene.rootNode.addChildNode(redTarget)
+    scene.scene.rootNode.addChildNode(purpleTarget)
+
+}
